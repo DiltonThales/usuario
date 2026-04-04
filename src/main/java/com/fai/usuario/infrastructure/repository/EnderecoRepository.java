0b4defd8bd5cd0e16e1b7884114cc19776d94a61
dilton <diltonthales.dt@gmail.com>
2026-04-04 19:03:55 -0300

    feat: adicionado método que salva dados usuário

--- src/main/java/com/fai/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/fai/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.fai.usuario.infrastructure.repository;
 
-import com.fai.aprendendospring.infrastructure.entity.Endereco;
+
+import com.fai.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
