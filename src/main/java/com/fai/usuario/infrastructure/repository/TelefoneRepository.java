eceb45bbd5b87cfcd646fb83fe14aa4d8d61eb67
dilton <diltonthales.dt@gmail.com>
2026-04-04 09:54:20 -0300

    feat: adicao das interfaces repository

---
+++ src/main/java/com/fai/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.fai.usuario.infrastructure.repository;
+
+import com.fai.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
